3c1f92227a88f1e2ba412b251b52e58bff3f387c
Sofiyan <ifren.sofiyan@gmail.com>
2025-02-10 16:48:39 +0300

    adding local notification word hourly

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,62 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_turkish_words_app/models/word.dart';
 import 'screens/main_screen.dart';
+import 'package:workmanager/workmanager.dart';
+import 'package:uuid/uuid.dart';
+import 'services/notification_service.dart';
+import 'data/data_words.dart';
 
 void main() {
+  // needed if you intend to initialize in the `main` function
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: false);
+
+  // Generate a unique identifier for the task
+  var uuid = Uuid();
+  String uniqueTaskId = uuid.v4();
+
+  // Register the periodic task with a unique ID
+  Workmanager().registerPeriodicTask(
+    uniqueTaskId,
+    "hourlyTask",
+    frequency: Duration(hours: 1),
+    constraints: Constraints(
+      networkType: NetworkType.connected,
+    ),
+  );
+
+  // Initialize the notification service
+  NotificationService.initialize();
+
   runApp(MultiWindowApp());
+}
+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
+    if (task == "hourlyTask") {
+      await showNotification();
+      return Future.value(true);
+    }
+    return Future.value(false);
+  });
+}
+
+Future<void> showNotification() async {
+  // Generate a random integer for the notification ID
+  final Random random = Random();
+  int notificationId = random.nextInt(10000);
+
+  // Select a random conjugation from the list
+  List<Word> words = await loadWords();
+  final Word randomWord = words[random.nextInt(words.length)];
+
+  // Use the NotificationService to show the notification
+  await NotificationService.showNotification(
+    id: notificationId,
+    title: randomWord.turkish,
+    body: randomWord.frenchMeaning,
+  );
 }
 
 class MultiWindowApp extends StatelessWidget {
---
+++ lib/services/notification_service.dart
@@ -0,0 +1,44 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  static final FlutterLocalNotificationsPlugin
+      _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  static Future<void> initialize() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  static Future<void> showNotification({
+    required int id,
+    required String title,
+    required String body,
+  }) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await _flutterLocalNotificationsPlugin.show(
+      id,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: id.toString(),
+    );
+  }
+}
